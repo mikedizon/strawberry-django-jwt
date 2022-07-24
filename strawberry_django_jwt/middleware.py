e19046b14635896abdf56dc3affa174b94798d6e
Michael Dizon <mdizon@gmail.com>
2022-07-24 11:51:50 -0400

    WIP

--- strawberry_django_jwt/middleware.py
+++ strawberry_django_jwt/middleware.py
@@ -1,7 +1,11 @@
+import json
+from jose import jwt
 from inspect import isawaitable
 from typing import Any, Set, cast
+from six.moves.urllib.request import urlopen
 
-from django.contrib.auth import authenticate
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.middleware import get_user
 from django.contrib.auth.models import AnonymousUser
 from django.utils.translation import gettext as _
@@ -19,11 +23,21 @@ from strawberry_django_jwt.utils import (
     get_token_argument,
 )
 
+AUTH0_RULES_NAMESPACE = settings.AUTH0['AUTH0_RULES_NAMESPACE']
+
+
 __all__ = [
     "allow_any",
+    "AuthError",
     "JSONWebTokenMiddleware",
     "AsyncJSONWebTokenMiddleware",
 ]
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
 
 
 def allow_any(info, **kwargs):
@@ -94,20 +108,73 @@ class BaseJSONWebTokenMiddleware(Extension):
 
 
 class JSONWebTokenMiddleware(BaseJSONWebTokenMiddleware):
+    def __init__(self):
+        self.cached_allow_any = set()
+        self.domain = settings.AUTH0['AUTH0_DOMAIN']
+        self.client_id = settings.AUTH0['AUTH0_CLIENT_ID']
+        self.audience = settings.AUTH0['AUTH0_AUDIENCE']
+        self.ALGORITHMS = settings.AUTH0["ALGORITHMS"]
+        self.jwks_url = 'https://{}/.well-known/jwks.json'.format(self.domain)
+        self.issuer = 'https://{}/'.format(self.domain)
+        self.jsonurl = urlopen("https://" + self.domain
+                               + "/.well-known/jwks.json")
+
     def resolve(self, _next, root, info: GraphQLResolveInfo, *args, **kwargs):
-        context, token_argument = self.resolve_base(info, **kwargs)
-
-        if (_authenticate(context) or token_argument is not None) and self.authenticate_context(info, **kwargs):
-
-            user = authenticate(request=context, **kwargs)
-
-            if user is not None:
-                context.user = user
-
-                if jwt_settings.JWT_ALLOW_ARGUMENT:
-                    self.cached_authentication.insert(info.path, user)
-
-        return _next(root, info, **kwargs)
+        context = info.context
+        auth = context.META.get(
+            settings.AUTH0['JWT_AUTH_HEADER_NAME'], "").split()
+        token = None
+        if len(auth) > 0:
+            token = auth[1]
+            jsonurl = urlopen(self.jwks_url)
+            jwks = json.loads(jsonurl.read())
+            unverified_header = jwt.get_unverified_header(token)
+            rsa_key = {}
+            for key in jwks["keys"]:
+                if key["kid"] == unverified_header["kid"]:
+                    rsa_key = {
+                        "kty": key["kty"],
+                        "kid": key["kid"],
+                        "use": key["use"],
+                        "n": key["n"],
+                        "e": key["e"]
+                    }
+            if rsa_key:
+                try:
+                    payload = jwt.decode(
+                        token,
+                        rsa_key,
+                        algorithms=self.ALGORITHMS,
+                        audience=self.audience,
+                        issuer="https://" + self.domain + "/"
+                    )
+                    sub = payload['sub']
+                    email = payload["{}/email".format(AUTH0_RULES_NAMESPACE)]
+                    user = get_user_model().objects.get_or_none(sub=sub)
+                    if user is not None:
+                        context.user = user
+                    elif sub is not None and email is not None:
+                        user, created = get_user_model().objects.get_or_create(email=email, sub=sub)
+                        if created:
+                            user.first_name = payload["{}/given_name".format(
+                                AUTH0_RULES_NAMESPACE)]
+                            user.last_name = payload["{}/family_name".format(
+                                AUTH0_RULES_NAMESPACE)]
+                            user.set_unusable_password()
+                            user.save()
+                        context.user = user
+                    else:
+                        context.user = AnonymousUser()
+                except jwt.ExpiredSignatureError:
+                    raise AuthError({"code": "token_expired",
+                                    "description": "token is expired"}, 401)
+                except Exception as e:
+                    raise AuthError({"code": "invalid_header",
+                                    "description":
+                                        "{}".format(e)}, 401)
+            else:
+                context.user = AnonymousUser()
+        return next(root, info, **kwargs)
 
 
 class AsyncJSONWebTokenMiddleware(BaseJSONWebTokenMiddleware):
